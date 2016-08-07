d3492d4206d3aec40d2ccd67ef347dcc5659324e
Ram Krishnan <kriyative@gmail.com>
2016-08-07 10:18:13 -0700

    Release 0.0.2 - upgrade nomad-0.0.2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject net.omnypay/nomad-migrators "0.0.1"
+(defproject net.omnypay/nomad-migrators "0.0.2"
   :encoding "utf-8"
   :description "OmnyPay Nomad migrators for H2 and PostgreSQL"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.h2database/h2 "1.4.192"]
-                 [net.omnypay/nomad "0.0.1"]
+                 [net.omnypay/nomad "0.0.2"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [org.postgresql/postgresql "9.4.1208"]])
