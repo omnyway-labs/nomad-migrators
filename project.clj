64354b0aa3a8c0eb44427facf3e4789cc9e98acd
icylisper <icylisper@gmail.com>
2016-08-24 09:12:01 -0700

    Add circle.yml; use SEMVER in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject net.omnypay/nomad-migrators "0.0.2"
+(def VERSION (or (not-empty (System/getenv "SEMVER")) "0.0.1-SNAPSHOT"))
+
+(defproject net.omnypay/nomad-migrators VERSION
   :encoding "utf-8"
   :description "OmnyPay Nomad migrators for H2 and PostgreSQL"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.h2database/h2 "1.4.192"]
-                 [net.omnypay/nomad "0.0.2"]
+                 [net.omnypay/nomad "0.0.3"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [org.postgresql/postgresql "9.4.1208"]])
