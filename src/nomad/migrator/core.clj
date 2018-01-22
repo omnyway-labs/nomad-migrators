eded6c81170494f29570bc32afc895e0838834f9
angusiguess <ethdem@gmail.com>
2018-01-22 16:26:24 -0400

    Bind migration with tx-db

--- src/nomad/migrator/core.clj
+++ src/nomad/migrator/core.clj
@@ -41,7 +41,8 @@
 
 (defn apply! [{:keys [db-spec]} tag migration-fn]
   (jdbc/with-db-transaction [db db-spec]
-    (migration-fn)
+    (binding [*current-connection* db]
+      (migration-fn))
     (jdbc/insert! db :nomad_schema_migrations
                   {:tag     (name tag)
                    :applied (date->timestamp
